e0f319835c78c75191c3ded9f1131220b5abeac3
Patricia Li <li.patricia@protonmail.com>
2021-01-24 01:37:23 -0500

    Filter search results

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ WORKSHEET_NAME = "Housing-RAW"
 sheet = Spreadsheet(SPREADSHEET_NAME)
 
 
-def search_craigslist(site, area, filters, geotagged):
+def search_craigslist(site, area, filters, sort_by=None, geotagged=None):
     craigslist = Craigslist()
 
-    results = craigslist.search(site=site, area=area, filters=filters, geotagged=geotagged)
+    results = craigslist.search(site=site, area=area, filters=filters, sort_by=sort_by, geotagged=geotagged)
     # craigslist.print_results(results)
 
     return results
 
-def get_current_results(spreadsheet_id, spreadsheet_range):
+def get_current_results():
     """Retrieves the IDs of the current listings in the spreadsheet
     """
     values = sheet.read_column(WORKSHEET_NAME, 1)
@@ -34,16 +34,36 @@ def generate_sheet_data(search_results, prev_listing_ids):
         if not columns and not prev_listing_ids: # only add columns if it is the first time running
             columns = list(result.keys())
             sheet_data.append(columns)
-        
+   
         if not result['id'] in prev_listing_ids: # remove duplicates
-            row = list(result.values())
-            sheet_data.append(row)
+            
+            if is_in_dc(result['url']):
+
+                if is_in_neighborhood(result['where']) != True: # filter results by neighborhood
+                    row = list(result.values())
+                    sheet_data.append(row)
 
         latest_listing_ids.append(result['id'])
 
     delete_expired_listings(latest_listing_ids, prev_listing_ids)
 
     return sheet_data
+
+def is_in_dc(url):
+    """Returns True if the listing is in Washington, D.C.
+    """
+    if "https://washingtondc.craigslist.org/doc/apa/d/washington" in url:
+        return True
+
+def is_in_neighborhood(where_col):
+    """Returns True if the listing is in certain neighborhoods
+    """
+    if where_col:
+        unwanted_neighborhoods = ['Petworth', 'Trinidad', 'Navy Yard', 'Mount Rainier', 'Van Ness'] # TODO: enhancement: NLP processing to standardize n-grams
+
+        for neighborhood in unwanted_neighborhoods:
+            if neighborhood in where_col:
+                return True
 
 def delete_expired_listings(latest_listing_ids, prev_listing_ids):
     """Deletes expired listings
@@ -67,21 +87,19 @@ def main():
         'laundry': 'w/d in unit',
         'parking': ['carport', 'attached garage', 'detached garage', 'off-street parking', 'street parking']
         }
+    sort_by = None # "price_asc", "price_desc"
     geotagged = False # TODO: enhancement: make the geotagging work with Sheets
 
-    # Define target spreadsheet metadata
-    spreadsheet_id = "1UfCp7ZucZ5RKJOVRBDQu7S-upFLIQGNXyxUsdavkFiQ" # Housing (Automated)
-    spreadsheet_range = "Housing-RAW"
-    value_input_option = "RAW"
+    # Perform search
+    results = search_craigslist(site, area, filters, sort_by, geotagged)
 
-    # Generate results
-    results = search_craigslist(site, area, filters, geotagged)
-    prev_listing_ids = get_current_results(spreadsheet_id, spreadsheet_range)
+    # Filter and prepare final result set
+    prev_listing_ids = get_current_results()
 
     sheet_data = generate_sheet_data(results, prev_listing_ids)
 
     # Append new results to spreadsheet
-    response = sheet.append_rows(WORKSHEET_NAME, sheet_data, value_input_option)
+    response = sheet.append_rows(WORKSHEET_NAME, sheet_data, "RAW")
     print(response)
 
 
