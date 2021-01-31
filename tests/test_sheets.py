b374480c5f84bea62ec21ea66c4ec204a89da41f
Patricia Li <li.patricia@protonmail.com>
2021-01-30 20:48:07 -0500

    Made unit tests independent

--- tests/test_sheets.py
+++ tests/test_sheets.py
@@ -1,69 +1,52 @@
 import pytest
-from unittest.mock import Mock, patch
+from unittest.mock import Mock, patch, PropertyMock
+import gspread
 from config import GoogleSheetsConfig
-from sheets import GoogleSheets
+from sheets import Spreadsheet
 
+
+SPREADSHEET_NAME = "Housing (Automated)"
+WORKSHEET_NAME = "Test"
 
 class TestGoogleSheets:
 
     @pytest.fixture
     def sheets(self):
-        config = GoogleSheetsConfig()
-        sheets = GoogleSheets(config)
+        sheets = Spreadsheet(SPREADSHEET_NAME)
+
+        sheets = Spreadsheet(SPREADSHEET_NAME)
+        type(sheets).spreadsheet = PropertyMock(return_value=Mock(spec=gspread.Spreadsheet))
 
         return sheets
-
-    # def test_read(self, sheets):
-    #     """Validate that a sheet can be read
-    #     """
-    #     spreadsheet_id = "1UfCp7ZucZ5RKJOVRBDQu7S-upFLIQGNXyxUsdavkFiQ" # Housing (Automated)
-    #     spreadsheet_range = "Test"
-
-    #     values = sheets.read(spreadsheet_id, spreadsheet_range)
-    #     # TODO: use a mock client
-
-    def test_append(self, sheets):
-        """Validate that a sheet can be appended to
-        """
-        spreadsheet_id = "1UfCp7ZucZ5RKJOVRBDQu7S-upFLIQGNXyxUsdavkFiQ" # Housing (Automated)
-        value_input_option = "RAW"
-        spreadsheet_range = "Test"
-        body = dict(
-            majorDimension='ROWS',
-            values=[[1, 2, 3], [1, 2, 3], [1, 2, 3]] # first list is columns
-        )
-
-        response = sheets.append(spreadsheet_id, value_input_option, spreadsheet_range, body)
-        print(response)
-        # TODO: use a mock client
-
-    def test_update(self, sheets):
-        """Validate that a sheet can be updated
-        """
-        spreadsheet_id = "1UfCp7ZucZ5RKJOVRBDQu7S-upFLIQGNXyxUsdavkFiQ" # Housing (Automated)
-        value_input_option = "RAW"
-        spreadsheet_range = "Test"
-        body = dict(
-            majorDimension='ROWS',
-            values=[[1, 2, 3], [1, 2, 3], [1, 2, 3]] # first list is columns
-        )
-
-        response = sheets.update(spreadsheet_id, value_input_option, spreadsheet_range, body)
-        print(response)
-        # TODO: use a mock client
 
     def test_read_column(self, sheets):
         """Validate that column values can be read
         """
-        spreadsheet_name = "Housing (Automated)"
-        worksheet_name = "Test"
-        index = 1
-        values = sheets.read_column(spreadsheet_name, worksheet_name, index)
+        column = 1
+        sheets.read_column(WORKSHEET_NAME, column)
 
-        print(values) # TODO: mock it
+        sheets.spreadsheet.worksheet().col_values.assert_called_once()
 
-    def test_append_gc(self, sheets):
-        sheet = sheets.gc.open("Housing (Automated)")
+    def test_append_rows(self, sheets):
+        """Validate that rows can be appended to a worksheet
+        """
+        values = [[1, 2, 3], [1, 2, 3], [1, 2, 3]]
+        value_input_option = "RAW"
 
-    def test_delete_gc(self, sheets):
-        pass
+        sheets.append_rows(WORKSHEET_NAME, values, value_input_option)
+
+        sheets.spreadsheet.worksheet().append_rows.assert_called_once()
+
+    def test_find_cell(self, sheets):
+        """Validate that a cell's location can be returned
+        """
+        sheets.find_cell(WORKSHEET_NAME, "1")
+
+        sheets.spreadsheet.worksheet().find.assert_called_once()
+
+    def test_delete_rows(self, sheets):
+        """Validate that a row can be deleted
+        """
+        sheets.delete_rows(WORKSHEET_NAME, 1)
+
+        sheets.spreadsheet.worksheet().delete_rows.assert_called_once()
